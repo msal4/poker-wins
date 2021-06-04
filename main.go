3b8e7dbe2808e9cd6dfe62763cdb70a49fabefa7
Mohammed Salman <msal4@outlook.com>
2021-06-04 15:18:20 +0300

    fix file permissions

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 const dbFileName = "game.db.json"
 
 func main() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 06666)
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
 	if err != nil {
 		log.Fatalf("problem opening %s, %v", dbFileName, err)
 	}
